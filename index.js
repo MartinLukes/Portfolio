2478bba5d71a3f7eb62e44003b163559d91346c4
MartinLukes <growwithlukes@gmail.com>
2024-10-28 14:48:55 +0500

    added

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#element", {
+  strings: ["Graphic Designer", "Web Developer", "Ethical Hacker","Video Editor","Gym Rat"],
+  typeSpeed: 50,
+  backspeed: 50,
+  backdelay: 500,
+  loop:true 
+});
